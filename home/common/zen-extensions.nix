3b29f09222988140577a9a48aedde0a4b09410b8
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-08-05 21:14:34 -0400

    Add Violentmonkey and Chameleon extensions to Zen

--- home/common/zen-extensions.nix
+++ home/common/zen-extensions.nix
@@ -10,7 +10,9 @@
       installation_mode = "force_installed";
     }) {
       "adnauseam@rednoise.org" = "adnauseam"; # AdNauseam
-      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = "vimium-ff"; # Vimium
+      "{3579f63b-d8ee-424f-bbb6-6d0ce3285e6a}" = "chameleon-ext"; # Chameleon
       "{d6f02b92-88b3-4aa5-ba7c-14519042171d}" = "sort-tabs-advanced"; # Sort Tabs Advanced
+      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = "vimium-ff"; # Vimium
+      "{aecec67f-0d10-4fa7-b7c7-609a2db280cf}" = "violentmonkey"; # Violentmonkey
     };
 }
