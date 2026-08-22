9f38b12b2a7f1c89d057ca5cafad097017119bab
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-08-22 14:05:06 -0400

    Open recent file or README on bare nvim launch

--- home/common/nvim/_files/lua/custom/plugins/snacks.lua
+++ home/common/nvim/_files/lua/custom/plugins/snacks.lua
@@ -1,5 +1,15 @@
 return {
   "folke/snacks.nvim",
+  init = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+      group = vim.api.nvim_create_augroup("custom-startup", { clear = true }),
+      once = true,
+      nested = true,
+      callback = require("custom.safe").config(function()
+        require("custom.startup").setup()
+      end),
+    })
+  end,
   opts = {
     dashboard = {
       sections = {
