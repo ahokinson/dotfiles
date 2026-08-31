81d53198aa7c952ab4e4c55c444d82cbd07d2510
ahokinson <1762048+ahokinson@users.noreply.github.com>
2026-08-30 23:10:23 -0400

    Move the Pharos readout into a tmux sidecard

--- home/common/tmux/default.nix
+++ home/common/tmux/default.nix
@@ -6,4 +6,9 @@
     terminal = "tmux-256color";
     extraConfig = builtins.readFile (selfPath "home/common/tmux/tmux.conf");
   };
+
+  xdg.configFile."tmux/pharos" = {
+    source = selfPath "home/common/tmux/_files/pharos";
+    executable = true;
+  };
 }
